fa8c54355d005ae34659068c5f15e824626e21c2
TobiasComp <tuvia1234@gmail.com>
2019-11-28 18:00:50 +0200

    advanced app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChocolateComponent } from './comps/chocolate/chocolate.component';
+import { IceCreamComponent } from './comps/ice-cream/ice-cream.component';
+import { DataTitleComponent } from './comps/data-title/data-title.component';
+import { CanActivateIceCreamLinkService } from './services/guards/can-activate-ice-cream-link.service';
+import { JsonUserComponent } from './comps/json-user/json-user.component';
+import { LoginComponent } from './comps/login/login.component';
+import { AmericansComponent } from './comps/americans/americans.component';
+import { NativeComponent } from './comps/Americans/native/native.component';
+import { BlacksComponent } from './comps/Americans/blacks/blacks.component';
+import { ImmigrantsComponent } from './comps/Americans/immigrants/immigrants.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'americans',children:[
+    { path:'', component:AmericansComponent},
+    { path: 'natives', component:NativeComponent},
+    { path: 'blacks', component:BlacksComponent},
+    { path: 'immigrants', component:ImmigrantsComponent},
+  ] },
+  { path: 'chocolate', component:ChocolateComponent},
+  { path: 'login', component:LoginComponent},
+  { path: 'ice-cream', component:IceCreamComponent, canActivate:[CanActivateIceCreamLinkService]},
+  { path: 'json-user/:id', component:JsonUserComponent},
+  { path: 'data-title', component:DataTitleComponent, data:{title:'what is going here!!!!'}},
+  { path: '', redirectTo:'/chocolate', pathMatch:'full'},
+  { path: '**', redirectTo:'/chocolate', pathMatch:'full'},
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChocolateComponent } from './comps/chocolate/chocolate.component';
+import { IceCreamComponent } from './comps/ice-cream/ice-cream.component';
+import { DataTitleComponent } from './comps/data-title/data-title.component';
+import { JsonUserComponent } from './comps/json-user/json-user.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './comps/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { AmericansComponent } from './comps/americans/americans.component';
+import { NativeComponent } from './comps/Americans/native/native.component';
+import { BlacksComponent } from './comps/Americans/blacks/blacks.component';
+import { ImmigrantsComponent } from './comps/Americans/immigrants/immigrants.component';
+import { BirthYearPipe } from './comps/birth-year.pipe';
+import { StudentsComponent } from './comps/students/students.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChocolateComponent,
+    IceCreamComponent,
+    DataTitleComponent,
+    JsonUserComponent,
+    LoginComponent,
+    AmericansComponent,
+    NativeComponent,
+    BlacksComponent,
+    ImmigrantsComponent,
+    BirthYearPipe,
+    StudentsComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
